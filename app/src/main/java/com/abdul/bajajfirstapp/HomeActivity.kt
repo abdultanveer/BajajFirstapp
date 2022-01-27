b1210f24c9cbe4d73d7d86d3c687fad2bf25758c
abdul <abdul.tanveer@gmail.com>
2022-01-27 12:23:09 +0530

    pass data b/w activities

--- app/src/main/java/com/abdul/bajajfirstapp/HomeActivity.kt
+++ app/src/main/java/com/abdul/bajajfirstapp/HomeActivity.kt
@@ -3,17 +3,23 @@ package com.abdul.bajajfirstapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        var name = intent.extras?.getString("sk")
+        var tvHome = findViewById<TextView>(R.id.tvHome)
+        tvHome.text = name
     }
 
     fun handleClick(view: android.view.View) {
         var intent: Intent    //kotlin says you can't have a variable as null
         intent = Intent(this,MainActivity::class.java)  //no need to give the word new
+        intent.putExtra("ds","divyang sharma")
         startActivity(intent)
     }
 }

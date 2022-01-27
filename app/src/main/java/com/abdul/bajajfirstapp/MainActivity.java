04113295a4f490ac11c4ecaf6a8f47fb02bb70e8
abdul <abdul.tanveer@gmail.com>
2022-01-27 11:53:52 +0530

    startActivity(intent)

--- app/src/main/java/com/abdul/bajajfirstapp/MainActivity.java
+++ app/src/main/java/com/abdul/bajajfirstapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.abdul.bajajfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -16,5 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickHandler(View view) {
         Toast.makeText(this, "loggin in", Toast.LENGTH_SHORT).show();
+        Intent mIntent;
+              mIntent  = new Intent(MainActivity.this,HomeActivity.class);
+        startActivity(mIntent);
+
     }
 }

73940f81f810be1d4325137c5000c1d2d6a03a7f
abdul <abdul.tanveer@gmail.com>
2022-01-28 09:51:36 +0530

    recyclerview part-3b

--- app/src/main/java/com/abdul/bajajfirstapp/CountriesAdapter.java
+++ app/src/main/java/com/abdul/bajajfirstapp/CountriesAdapter.java
@@ -1,7 +1,40 @@
 package com.abdul.bajajfirstapp;
 
 
-import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class CountriesAdapter extends RecyclerView.Adapter<CountriesViewHolder> {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+//step 1 -- implemented the methods
+//step 2 - created a class CountriesViewHolder [import]
+//step 3 - extend CountriesViewHolder with  RecyclerView.ViewHolder
+//4 -- create constructor for CountriesViewHolder
+
+public class CountriesAdapter extends RecyclerView.Adapter<CountriesAdapter.CountriesViewHolder> {
+
+
+    
+    @NonNull
+    @Override
+    public CountriesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CountriesViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class  CountriesViewHolder extends RecyclerView.ViewHolder{
+
+        public CountriesViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }

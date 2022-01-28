f136f4f39e9dd0e910a0baf19f1693f498b58d5b
abdul <abdul.tanveer@gmail.com>
2022-01-28 09:45:27 +0530

    recyclerview part-2 [data,recyclerview]

--- app/src/main/java/com/abdul/bajajfirstapp/RecyclerActivity.java
+++ app/src/main/java/com/abdul/bajajfirstapp/RecyclerActivity.java
@@ -1,14 +1,18 @@
 package com.abdul.bajajfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class RecyclerActivity extends AppCompatActivity {
 
+    String[] countries = new String[]{"Russia","Ukraine","UK","Germany","US","China"}; //data = plug
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recycler);
+        setContentView(R.layout.activity_recycler);   //inflating activity_recyclerview layoutinflater
+        RecyclerView countriesRecyclerView = findViewById(R.id.countriesRview); //socket
     }
 }

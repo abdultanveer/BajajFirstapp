6bbabdffb6683e7766d6b43bf5e0c2ed29fccaf7
abdul <abdul.tanveer@gmail.com>
2022-01-31 10:13:41 +0530

    runtime permissions

--- app/src/main/java/com/abdul/bajajfirstapp/ContentProviderActivity.kt
+++ app/src/main/java/com/abdul/bajajfirstapp/ContentProviderActivity.kt
@@ -1,31 +1,58 @@
 package com.abdul.bajajfirstapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.widget.CursorAdapter
 import android.widget.ListView
 import android.widget.SimpleCursorAdapter
+import androidx.appcompat.app.AppCompatActivity
 
-class ContentProviderActivity : AppCompatActivity() {
+
+class ContentProviderActivity : BasePermissionAppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content_provider)
 
         val uriSms: Uri = Uri.parse("content://sms/inbox")
-        val dataCursor: Cursor = getContentResolver()                   //contentresolver will bring up the db & table into mem
-            .query(uriSms, null, null, null, null)!! //selection -- rows, projectioin --columns
+
+
+        //getReadSMSPermission(requestPermissionAction(this))
+        val dataCursor: Cursor =
+            getContentResolver()                   //contentresolver will bring up the db & table into mem
+                .query(uriSms, null, null, null, null)!! //selection -- rows, projectioin --columns
         //select * from inbox
         var from = arrayOf("body")
         var to = intArrayOf(android.R.id.text1)
-        var adapter = SimpleCursorAdapter(this,
-                                            android.R.layout.simple_list_item_1,
-            dataCursor ,from,to)
+        var adapter = SimpleCursorAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            dataCursor, from, to
+        )
 
         var cpListView: ListView = findViewById(R.id.cpListview)
         cpListView.adapter = adapter
 
 
     }
+
+
+    val requestPermissionAction =  object : RequestPermissionAction{
+        override fun permissionGranted() {
+        }
+
+        override fun permissionDenied() {
+        }
+    }
+
+
+
 }
+
+
+
+
